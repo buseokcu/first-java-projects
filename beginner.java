9b684a345157761cf611d46d858e90b232b76392
Buse Okcu <98065397+buseokcu@users.noreply.github.com>
2022-10-31 00:51:52 +0300

    Update and rename beginner to beginner.java

---
+++ beginner.java
@@ -0,0 +1,27 @@
+package thefirstfirstproject;
+
+public class thefirstclass {
+
+	public static void main(String[] args) {
+		
+		int a = 51;
+		short s = 32;
+		double d = 3.14;
+		char c = 'A';
+		boolean b = true;
+		String buse = "Liebe";
+		
+		System.out.println("A'nın değeri " + a + " dir");
+		System.out.println(s);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(b);
+		System.out.println(buse);
+		
+		
+		System.out.println();
+		
+		System.out.println ("ich lerne JAVA");
+	}
+	
+}
